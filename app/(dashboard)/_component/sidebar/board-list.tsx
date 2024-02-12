b0a4d3a055bf1e052f33801b2c38f78756216bb0
asura69 <you@example.com>
2024-02-12 19:50:28 +0530

    Made board images visible, stopped at Time stamp 2:53:04

--- app/(dashboard)/_component/sidebar/board-list.tsx
+++ app/(dashboard)/_component/sidebar/board-list.tsx
@@ -7,6 +7,7 @@ import { api } from "@/convex/_generated/api";
 import { EmptySearch } from "../empty-search";
 import { EmptyFavourites } from "../empty-favourites";
 import { EmptyBoards } from "../empty-boards";
+import { BoardCard } from "../board-card";
 
 interface BoardListProps  {
     orgId: string;
@@ -49,7 +50,24 @@ export const BoardList = ({
 
     return (
         <div>
-            {JSON.stringify(data)}
+            <h2 className="text-3xl">
+                {query.favourites ?  "Favourite boards" : "Team boards" }
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+                {data?.map((board)=>(
+                    <BoardCard
+                        key = {board._id}
+                        id = {board._id}
+                        title = {board.title}
+                        imageUrl = {board.ImageUrl}
+                        authorId = {board.authorId}
+                        authorName = {board.authorName}
+                        createdAt = {board._creationTime}
+                        orgId = {board.orgId}
+                        isFavourite = {false}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
