137ce38f14aaf6ca0f4f303afea1d8c1b92e3096
asura69 <you@example.com>
2024-02-14 19:04:47 +0530

    Board addition

---
+++ app/(dashboard)/_component/new-board-button.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { api } from "@/convex/_generated/api"
+import { useApiMutation } from "@/hooks/use-api-mutation"
+import { cn } from "@/lib/utils"
+import { Plus } from "lucide-react"
+import { toast } from "sonner"
+
+interface NewBoardButtonProps {
+    orgId: string
+    disabled: boolean
+}
+
+export const NewBoardButton = ({
+    orgId,
+    disabled
+}:NewBoardButtonProps) => {
+
+    const {mutate, pending} = useApiMutation(api.board.create);
+
+    const onClick = () => {
+        mutate({
+            orgId,
+            title: "Untitled"
+        })
+        .then((id)=>{
+            toast.success("Board created!")
+        })
+        .catch(()=> toast.error("Failed to create board."));
+    }
+
+    return (
+        <button
+            onClick={onClick}
+            disabled = {pending || disabled}
+            className={cn(
+                "col-span-1 aspect-[100/127] bg-yellow-50 rounded-lg hover:bg-yellow-100 flex flex-col items-center justify-center py-6",
+                (disabled || pending) && "opacity-75 bg-yellow-50 cursor-not-allowed"
+            )}
+        >
+            <div />
+            <Plus 
+                className="h-12 w-12 stroke-1"
+            />
+            <p className="text-sm font-light">
+                New Board
+            </p>
+        </button>
+    )
+}
