d2b3a3eeeca5467c7865e77cec989146cf77bb4d
asura69 <you@example.com>
2024-02-19 16:19:19 +0530

    Stopped 5:19:50

--- app/board/[boardId]/_componenets/canvas.tsx
+++ app/board/[boardId]/_componenets/canvas.tsx
@@ -4,6 +4,7 @@ import { Info } from "./info"
 import { Participants } from "./participants"
 import { ToolBar } from "./toolbar"
 
+import { useSelf } from "@/liveblocks.config"
 
 interface CanvasProps {
     boardId: string
@@ -11,6 +12,7 @@ interface CanvasProps {
 export const Canvas = ({
     boardId
 }: CanvasProps) => {
+    const info = useSelf((me)=>me.info);
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
             <Info />
