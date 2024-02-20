8a030368480897189445b7a5c9e831389e4d48cd
asura69 <you@example.com>
2024-02-20 18:19:24 +0530

    stopped at 5:36

--- app/board/[boardId]/_componenets/canvas-loading.tsx
+++ app/board/[boardId]/_componenets/canvas-loading.tsx
@@ -1,6 +1,6 @@
 import { Loader } from "lucide-react";
-import { Info } from "./info";
-import { Participants } from "./participants";
+import { InfoSkeleton } from "./info";
+import { ParticipantsSkeleton } from "./participants";
 
  export const Loading = () => {
     return (
@@ -8,8 +8,8 @@ import { Participants } from "./participants";
             className="h-full w-full relative bg-neutral-100 touch-none flex items-center justify-center"
         >
             <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
-            <Info.Skeleton />
-            <Participants.Skeleton />
+            <InfoSkeleton />
+            <ParticipantsSkeleton />
         </main>
     )
  }
